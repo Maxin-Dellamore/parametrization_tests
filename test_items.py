a1ff54a9212f21ecc93066bb6ac1b899968e3e64
Maxin-Dellamore <Maxin567@yandex.ru>
2024-10-04 00:24:24 +0300

    Загружаем в репозиторий

--- test_items.py
+++ test_items.py
@@ -1,10 +1,9 @@
 import time
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
-
-#time.sleep(30)
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_page_should_contain_add_to_basket_button(browser):
     browser.get(link)
+    time.sleep(30)
     assert len(browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')) > 0, 'button not found'
