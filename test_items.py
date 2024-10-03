7911081a487c701c5dd194302a41d4ef48e0d012
Maxin-Dellamore <Maxin567@yandex.ru>
2024-10-03 23:23:37 +0300

    Загружаем тесты в репозиторий

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
+
+#time.sleep(30)
+
+def test_page_should_contain_add_to_basket_button(browser):
+    browser.get(link)
+    assert len(browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')) > 0, 'button not found'
